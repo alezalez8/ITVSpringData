60f1ef8d83c20d5116aaceaa308fe883a80769fb
alezalez8 <5446943@ukr.net>
2021-11-10 16:13:59 +0200

    lesson 3 24.00

--- src/main/java/itvdn/todolist/controllers/UserController.java
+++ src/main/java/itvdn/todolist/controllers/UserController.java
@@ -6,7 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class UserController {
@@ -18,34 +19,8 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping(path = "/create_table")
-    public ResponseEntity<String> getTest() {
-        userService.createTableUser();
-        // userService.createUser(new User());
-        return new ResponseEntity<>("Table has been created", HttpStatus.OK);
-    }
-
-    @PostMapping(path = "/user/create")
-    public ResponseEntity<Integer> createUser(@RequestBody User user) {
-        int result = userService.createUser(user);
-        return new ResponseEntity<>(result, HttpStatus.CREATED);
-    }
-
-    @GetMapping(path = "/user/get/{id}")
-    public ResponseEntity<User> getUser(@PathVariable long id) {
-        User result = userService.getUser(id);
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
-
-    @PutMapping(path = "user/update/{id}")
-    public ResponseEntity<Integer> updateUser(@RequestBody User updatedUser, @PathVariable long id) {
-        int result = userService.updateUser(updatedUser, id);
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
-
-    @DeleteMapping(path = "/user/delete/{id}")
-    public ResponseEntity<Integer> deleteUser(@PathVariable long id) {
-        int result = userService.deleteUser(id);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+    @PostMapping(path = "/user")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
