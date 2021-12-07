2cf127bebd1a97ab38d8ea48def6ffe0a55b8276
alezalez8 <5446943@ukr.net>
2021-12-07 20:05:04 +0200

    lesson 9 25.28

--- src/test/java/itvdn/todolist/services/UserServiceTest.java
+++ src/test/java/itvdn/todolist/services/UserServiceTest.java
@@ -4,10 +4,14 @@ package itvdn.todolist.services;
 import itvdn.todolist.domain.PlainObjects.UserPojo;
 import itvdn.todolist.domain.User;
 import itvdn.todolist.services.interfaces.IUserService;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -25,6 +29,11 @@ public class UserServiceTest {
         this.userService = applicationContext.getBean("userService", IUserService.class);
     }
 
+    @After
+    public void cleanDB() {
+        userService.getAllUsers().forEach(userPojo -> userService.deleteUser(userPojo.getId()));
+    }
+
     @Test
     public void createUserTest() {
         User newUser = new User();
@@ -36,7 +45,25 @@ public class UserServiceTest {
         assertEquals(EMAIL, actual.getEmail());
         assertEquals(PASSWORD, actual.getPassword());
         assertNotNull(actual.getId());
+    }
 
+    @Test
+    public void deleteUserTest() {
+        User newUser = new User();
+        newUser.setEmail(EMAIL);
+        newUser.setPassword(PASSWORD);
+
+        UserPojo actual = userService.createUser(newUser);
+        List<UserPojo> userAfterCreate = userService.getAllUsers();
+        assertEquals(1, userAfterCreate.size());
+
+        userService.deleteUser(actual.getId());
+
+       // userService.getAllUsers().forEach(userPojo -> userService.deleteUser(userPojo.getId()));
+        List<UserPojo> userListAfterDelete = userService.getAllUsers();
+        assertEquals(0, userListAfterDelete.size());
 
     }
+
+
 }
